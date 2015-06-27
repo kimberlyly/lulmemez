3046a058724b66b042ef62ad052104445913bce4
h3yin <h3yin@ucsd.edu>
2015-06-26 22:29:48 -0700

    change images to folders

--- app/src/main/java/hackmobile/lulmemez/util/Modify_Img.java
+++ app/src/main/java/hackmobile/lulmemez/util/Modify_Img.java
@@ -1,8 +1,10 @@
 package hackmobile.lulmemez.util;
 
+import hackmobile.lulmemez.R;
+
 /**
  * Created by Kevin on 6/26/2015.
  */
 public class Modify_Img {
-    
+    Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
 }
