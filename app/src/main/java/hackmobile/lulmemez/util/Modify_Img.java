a9f435ada320d4942cc731459f50f240ee213715
h3yin <h3yin@ucsd.edu>
2015-06-26 22:36:11 -0700

    what the
    fu

--- app/src/main/java/hackmobile/lulmemez/util/Modify_Img.java
+++ app/src/main/java/hackmobile/lulmemez/util/Modify_Img.java
@@ -1,10 +1,13 @@
 package hackmobile.lulmemez.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import hackmobile.lulmemez.R;
 
 /**
  * Created by Kevin on 6/26/2015.
  */
 public class Modify_Img {
-    Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
+    //Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.image);
 }
